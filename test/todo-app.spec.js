89acb67ae8d6f7f6f9f509aaf013335be2a1476f
Syed-Umair <syed.yousuff@anywhere.co>
2020-12-18 21:11:00 +0530

    jest config

--- test/todo-app.spec.js
+++ test/todo-app.spec.js
@@ -2,7 +2,7 @@ describe('ToDo-App', () => {
   test('should display Todo App text', () => {
     const element = window.document.createElement('todo-app');
     window.document.body.appendChild(element);
-    const renderedText = element.textContent;
-    expect(renderedText).toContain('Todo App');
+    const renderedText = element.firstChild.textContent;
+    expect(renderedText).toBe('Todo App');
   });
 });
