43fb0a103f6e476bd3b933ef7d0ca3189306b628
Syed-Umair <syed.yousuff@anywhere.co>
2020-12-18 21:15:41 +0530

    fix test cases

--- test/todo-app.spec.js
+++ test/todo-app.spec.js
@@ -2,7 +2,7 @@ describe('ToDo-App', () => {
   test('should display Todo App text', () => {
     const element = window.document.createElement('todo-app');
     window.document.body.appendChild(element);
-    const renderedText = element.firstChild.textContent;
+    const renderedText = element.children[0].innerText;
     expect(renderedText).toBe('Todo App');
   });
 });
